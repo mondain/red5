29e14945ac51d39f3fe891c2cd65129f97a77b52
daccattato <daccattato@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2005-10-06 02:42:20 +0000

    initial commit

---
+++ src/org/red5/server/io/flv/FLVEncoder.java
@@ -0,0 +1,13 @@
+package org.red5.server.io.flv;
+
+public class FLVEncoder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
