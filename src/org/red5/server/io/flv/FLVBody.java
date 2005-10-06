71f88e5df18bc6f26137082e6ae457e08d1e6457
daccattato <daccattato@gmail.com>
2005-10-06 02:41:25 +0000

    initial commit

--- src/org/red5/server/io/flv/FLVBody.java
+++ src/org/red5/server/io/flv/FLVBody.java
@@ -1,5 +1,19 @@
 package org.red5.server.io.flv;
 
-public interface FLVBody {
-
+public class FLVBody {
+	private int previousTagSize;
+	private FLVTag tag;
+	
+	public int getPreviousTagSize() {
+		return previousTagSize;
+	}
+	public void setPreviousTagSize(int previousTagSize) {
+		this.previousTagSize = previousTagSize;
+	}
+	public FLVTag getTag() {
+		return tag;
+	}
+	public void setTag(FLVTag tag) {
+		this.tag = tag;
+	}
 }
