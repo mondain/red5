4fc4bb1a977df1a2372983ee5e058c165133786a
daccattato <daccattato@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2005-10-06 04:06:41 +0000

    initial codebase

--- src/org/red5/server/io/flv/FLVDecoder.java
+++ src/org/red5/server/io/flv/FLVDecoder.java
@@ -1,13 +1,84 @@
 package org.red5.server.io.flv;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class FLVDecoder {
 
+	private String fileName = null;
+	FileInputStream fis = null;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if(args.length < 1) {
+			usage();
+			return;
+		}
+		
+		FLVDecoder decoder = new FLVDecoder(args[0]);
+		
+		
+		decoder.decode();
+	}
+	
+	private static void usage() {
+		// TODO Auto-generated method stub
+		System.out.println("java FLVDecoder.java [filename]");
+	}
 
+	public FLVDecoder(String s) {
+		this.fileName = s;
+		try {
+			fis = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//decode();
+	}
+	
+	public void decode() {
+		if(fileName == null) {
+			System.out.println("Please enter a filename");
+		}
+		FLVHeader header = new FLVHeader();
+		
+		int signatureBytes = 3;
+		byte b[] = new byte[3];
+		for(int i=0; i<signatureBytes; i++) {
+			try {
+				b[i] = (byte) fis.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		header.setSIGNATURE(b);
+		System.out.println("Header Signature:" + header.getSIGNATURE());
+		
+		
+		
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public FileInputStream getFis() {
+		return fis;
+	}
+
+	public void setFis(FileInputStream fis) {
+		this.fis = fis;
 	}
 
 }
--- src/org/red5/server/io/flv/FLVHeader.java
+++ src/org/red5/server/io/flv/FLVHeader.java
@@ -14,6 +14,76 @@ public class FLVHeader {
 	
 	// DATA OFFSET
 	// reserved for data up to 4,294,967,295
-	public int DATA_OFFSET = 0x09;
+	public int DATA_OFFSET;
+
+	public int getDATA_OFFSET() {
+		return DATA_OFFSET;
+	}
+
+	public void setDATA_OFFSET(int data_offset) {
+		DATA_OFFSET = data_offset;
+	}
+
+	public String getSIGNATURE() {
+		String ret = "";
+		char[] tmpChar = new char[3];
+		for(int i=0; i<SIGNATURE.length; i++) {
+			tmpChar[i] = (char) SIGNATURE[i];
+		}
+		
+		
+		
+		//ret = tmpChar[0] + tmpChar[1] + tmpChar[2];
+		
+		for(int i=0; i<3; i++) {
+			ret += tmpChar[i];	
+		}
+		
+		return ret;
+	}
+
+	public void setSIGNATURE(byte[] signature) {
+		SIGNATURE = signature;
+	}
+
+	public byte getTYPE_FLAGS_AUDIO() {
+		return TYPE_FLAGS_AUDIO;
+	}
+
+	public void setTYPE_FLAGS_AUDIO(byte type_flags_audio) {
+		TYPE_FLAGS_AUDIO = type_flags_audio;
+	}
+
+	public byte getTYPE_FLAGS_RESERVED01() {
+		return TYPE_FLAGS_RESERVED01;
+	}
+
+	public void setTYPE_FLAGS_RESERVED01(byte type_flags_reserved01) {
+		TYPE_FLAGS_RESERVED01 = type_flags_reserved01;
+	}
+
+	public byte getTYPE_FLAGS_RESERVED02() {
+		return TYPE_FLAGS_RESERVED02;
+	}
+
+	public void setTYPE_FLAGS_RESERVED02(byte type_flags_reserved02) {
+		TYPE_FLAGS_RESERVED02 = type_flags_reserved02;
+	}
+
+	public byte getTYPE_FLAGS_VIDEO() {
+		return TYPE_FLAGS_VIDEO;
+	}
+
+	public void setTYPE_FLAGS_VIDEO(byte type_flags_video) {
+		TYPE_FLAGS_VIDEO = type_flags_video;
+	}
+
+	public byte getVERSION() {
+		return VERSION;
+	}
+
+	public void setVERSION(byte version) {
+		VERSION = version;
+	}
 
 }
