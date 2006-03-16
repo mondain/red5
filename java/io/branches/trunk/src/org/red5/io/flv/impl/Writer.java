9eb01a6c6fc155f9cc3d5601165343473c05d569
daccattato <daccattato@gmail.com>
2006-03-16 19:49:55 +0000

    creating branch for FLV Meta Data

---
+++ java/io/branches/trunk/src/org/red5/io/flv/impl/Writer.java
@@ -0,0 +1,201 @@
+package org.red5.io.flv.impl;
+
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright © 2006 by respective authors. All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ * 
+ * @author The Red5 Project (red5@osflash.org)
+ * @author Dominick Accattato (daccattato@gmail.com)
+ * @author Luke Hubbard, Codegent Ltd (luke@codegent.com)
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteOrder;
+
+import org.apache.mina.common.ByteBuffer;
+import org.red5.io.flv.IFLV;
+import org.red5.io.flv.ITag;
+import org.red5.io.flv.IWriter;
+import org.red5.io.utils.IOUtils;
+
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * A Writer is used to write the contents of a FLV file
+ * 
+ * @author The Red5 Project (red5@osflash.org)
+ * @author Dominick Accattato (daccattato@gmail.com)
+ * @author Luke Hubbard, Codegent Ltd (luke@codegent.com)
+ * @version 0.3
+ */
+public class Writer implements IWriter {
+	
+	private FileOutputStream fos = null;
+	private WritableByteChannel channel;
+	private MappedByteBuffer mappedFile;
+	private ByteBuffer out;
+	private ITag lastTag = null;
+	
+	private IFLV flv = null;
+	private long bytesWritten = 0;
+	private int offset = 0;
+	
+	public Writer(FileOutputStream fos){
+		this(fos,null);
+	}
+	
+	/**
+	 * WriterImpl Constructor
+	 * @param fos 
+	 */
+	public Writer(FileOutputStream fos, ITag lastTag) {
+		this.fos = fos;
+		this.lastTag = lastTag;
+		if(lastTag !=null) offset=lastTag.getTimestamp();
+		channel = this.fos.getChannel();
+		out = ByteBuffer.allocate(1024);
+		out.setAutoExpand(true);
+	}
+
+	/**
+	 * Writes the header bytes
+	 * @throws IOException 
+	 *
+	 */
+	public void writeHeader() throws IOException {
+		// TODO Auto-generated method stub
+		
+		out.put((byte)0x46);
+		out.put((byte)0x4C);
+		out.put((byte)0x56);
+		
+		// Write version
+		out.put((byte)0x01);
+		
+		// For testing purposes write video only
+		// TODO CHANGE
+		// out.put((byte)0x08);
+		// NOTE (luke): I looked at the docs on the wiki and it says it should be 0x05 for audio and video
+		// I think its safe to assume it will be both
+		out.put((byte)0x05);
+		
+		// Data Offset
+		out.putInt(0x09);
+		
+		out.flip();
+		
+		channel.write(out.buf());
+	
+	}
+
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#getFLV()
+	 */
+	public IFLV getFLV() {
+		return flv;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#setFLV()
+	 */
+	public void setFLV(IFLV flv) {
+		this.flv = flv;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#getOffset()
+	 */
+	public int getOffset() {
+		// TODO Auto-generated method stub
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#getBytesWritten()
+	 */
+	public long getBytesWritten() {
+		return bytesWritten; 
+	}
+
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#writeTag(org.red5.io.flv.Tag)
+	 */
+	public boolean writeTag(ITag tag) throws IOException {
+		// PreviousTagSize
+		//out = out.reset();
+		out.clear();
+		
+		out.putInt((lastTag == null) ? 0 : (lastTag.getBodySize() + 11));
+		
+		// Data Type
+		out.put((byte)(tag.getDataType()));
+		
+		// Body Size
+		IOUtils.writeMediumInt(out, tag.getBodySize());
+		
+		// Timestamp
+		IOUtils.writeMediumInt(out, tag.getTimestamp() + offset);
+		
+		// Reserved
+		out.putInt(0x00);
+
+		out.flip();
+		bytesWritten += channel.write(out.buf());
+
+		ByteBuffer bodyBuf = tag.getBody();
+		bytesWritten += channel.write(bodyBuf.buf());
+		
+		lastTag = tag;
+		
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.red5.io.flv.Writer#writeTag(byte, java.nio.ByteBuffer)
+	 */
+	public boolean writeTag(byte type, ByteBuffer data) throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc) 
+	 * @see org.red5.io.flv.Writer#close()
+	 */
+	public void close() {
+		try {
+			channel.close();
+			fos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public boolean writeStream(byte[] b) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ java/io/branches/trunk/tests/org/red5/server/io/test/MetaDataInjectionTest.java
@@ -0,0 +1,179 @@
+package org.red5.server.io.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.TreeSet;
+
+import org.apache.mina.common.ByteBuffer;
+import org.red5.io.amf.Output;
+import org.red5.io.flv.IFLV;
+import org.red5.io.flv.IFLVService;
+import org.red5.io.flv.impl.FLVService;
+import org.red5.io.flv.impl.MetaData;
+import org.red5.io.flv.IReader;
+import org.red5.io.flv.ITag;
+import org.red5.io.flv.impl.Tag;
+import org.red5.io.flv.IWriter;
+import org.red5.io.object.Deserializer;
+import org.red5.io.object.Serializer;
+import org.red5.io.utils.IOUtils;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class MetaDataInjectionTest extends TestCase {
+
+	private IFLVService service;
+	
+	/**
+	 * SetUp is called before each test
+	 * @return void
+	 */
+	public void setUp() {
+		service = new FLVService();
+		service.setSerializer(new Serializer());
+		service.setDeserializer(new Deserializer());
+	}
+	
+	/*
+	public void testMetaDataInjection() throws IOException {
+		File f = new File("tests/test_cue3.flv");
+		
+		if(f.exists()) {			
+			f.delete();
+		}
+		
+		// Create new file
+		f.createNewFile();
+		FileOutputStream fos = new FileOutputStream(f);
+		//fos.write((byte)0x01);
+		FLV flv = service.getFLV(fos);
+		Writer writer = flv.writer();
+		
+		// Create a reader for testing
+		File readfile = new File("tests/test_cue.flv");
+		FileInputStream fis = new FileInputStream(readfile);
+		FLV readflv = service.getFLV(fis);
+		Reader reader = readflv.reader();
+		
+		writeTagsWithInjection(reader, writer);		
+	}
+	*/
+
+	
+	private void writeTagsWithInjection(IReader reader, IWriter writer) throws IOException {
+		ITag tag = null;
+		
+		MetaData mdi1 = new MetaData("mdi1");
+		mdi1.setTimestamp(100);
+		mdi1.put("name", "test1");
+		
+		MetaData mdi2 = new MetaData("mdi2");
+		mdi2.setTimestamp(150);
+		mdi2.put("name", "test2");
+		
+		MetaData mdi3 = new MetaData("mdi3");
+		mdi3.setTimestamp(300);
+		mdi3.put("name", "test3333");
+		
+		MetaData mdi4 = new MetaData("mdi3");
+		mdi4.setTimestamp(300);
+		mdi4.put("name", "test3333");
+		
+		Object o = new Object();
+		
+		// Place in Treeset for sorting
+		TreeSet ts = new TreeSet();
+		ts.add(mdi1);
+		ts.add(mdi2);
+		ts.add(mdi3);
+						
+		System.out.println("ts: " + ts);
+		int tmpTimeStamp = ((MetaData) ts.first()).getTimestamp();
+		Tag injectedTag = null;
+		while(reader.hasMoreTags()) {
+			tag = reader.readTag();
+			
+			if(!ts.isEmpty()) {
+				
+				while(tag.getTimestamp() > tmpTimeStamp) {
+					injectedTag = (Tag) injectMetaData(ts.first(), writer, tag);
+					writer.writeTag(injectedTag);
+					System.out.println("BodySize: " + injectedTag.getBodySize());
+					
+					tag.setPreviousTagSize((injectedTag.getBodySize() + 11));
+					ts.remove(ts.first());
+					if(ts.isEmpty()) {
+						break;						
+					}
+					tmpTimeStamp = ((MetaData) ts.first()).getTimestamp();
+				}
+				
+			}
+			//System.out.println("injectedTag: " + injectedTag);
+			writer.writeTag(tag);
+			
+			//printTag(tag);
+		}
+		
+	}
+		
+	private ITag injectMetaData(Object mdi, IWriter writer, ITag tag) {
+		System.out.println("in inject");
+		
+		ITag retTag = null;
+		
+		byte tmpDataType = 0x00;
+		int tmpTimestamp = 0;
+		int tmpBodySize = 0;
+		ByteBuffer tmpBody = null;
+		int tmpPreviousTagSize = 0;
+		
+		ByteBuffer buf = ByteBuffer.allocate(1000);
+		Output out = new Output(buf);
+		Serializer ser = new Serializer();
+				
+		MetaData tmpMdi = (MetaData) mdi;
+		
+		out.buf().clear();
+		ser.serialize(out, tmpMdi);
+						
+		tmpBody = out.buf().flip();		
+		tmpBodySize = out.buf().limit();	
+		tmpPreviousTagSize = tag.getPreviousTagSize();
+		tmpDataType = ((byte)(ITag.TYPE_METADATA));
+		tmpTimestamp = tmpMdi.getTimestamp();
+
+		
+		retTag = new Tag(tmpDataType, tmpTimestamp, tmpBodySize, tmpBody, tmpPreviousTagSize);
+		System.out.println("tmpBody: " + tmpBody);
+		System.out.println("tmpBodySize: " + tmpBodySize);
+		System.out.println("tmpDataType: " + tmpDataType);
+		System.out.println("tmpTimestamp: " + tmpTimestamp);
+		
+		return retTag;
+	}
+
+	public void testMetaDataOrder() {
+		MetaData mdi1 = new MetaData("mdi1");
+		mdi1.setTimestamp(100);
+		
+		MetaData mdi2 = new MetaData("mdi2");
+		mdi2.setTimestamp(50);
+		
+		MetaData mdi3 = new MetaData("mdi3");
+		mdi3.setTimestamp(0);
+		
+		TreeSet ts = new TreeSet();
+		ts.add(mdi1);
+		ts.add(mdi2);
+		ts.add(mdi3);
+		
+		System.out.println("ts: " + ts);
+		
+		Assert.assertEquals(true, true);
+	}
+	
+}
