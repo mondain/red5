39363cc6a1c045452832dcc9839aaec55e8b312e
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-04-02 20:25:34 +0000

    updated windows installer so network ports can be configured switched back to Inno Setup because of way easier extensibility than BitRock

--- build/installer.py
+++ build/installer.py
@@ -3,19 +3,12 @@
 ##
 ##
 import os, sys
-import tempfile
-from distutils.dir_util import remove_tree
+from time import strftime
 
 # defaults
-SVN_CMD  = r'C:\cygwin\bin\svn.exe'
 ANT_CMD  = r'D:\Temp\apache-ant-1.6.5\bin\ant.bat'
-BITROCK_CMD = r'C:\Programme\BitRock InstallBuilder Enterprise 3.5.1\bin\builder.exe'
-VERSION  = '0.3'
-
-REPOSITORIES = {
-    #'red5_io': 'http://svn1.cvsdude.com/osflash/red5/java/io/trunk/',
-    'red5_server': 'http://svn1.cvsdude.com/osflash/red5/java/server/tags/0.3/',
-}
+INNO_CMD = r'C:\Programme\InnoSe~2\ISCC.exe'
+VERSION  = strftime('%Y%m%d')
 
 def error(msg):
     print 'ERROR: %s' % msg
@@ -31,48 +24,41 @@ class Builder:
         args = args and ' ' + ' '.join(args) or ''
         assert os.system('%s -quiet -buildfile %s%s' % (ant, script, args)) == 0
 
-    def __init__(self, java_home, ant_cmd, workdir=None):
-        if workdir is None:
-            workdir = tempfile.mkdtemp('red5')
-        self.workdir = workdir
+    def __init__(self, java_home, ant_cmd):
         self.java_home = java_home
         self.ant_cmd = ant_cmd
         self.build_root = os.path.abspath(os.path.split(__file__)[0])
 
-    def checkout(self, url, path):
-        log('Checking out of %s' % url)
-        assert os.system("%s export %s %s" % (SVN_CMD, url, path)) == 0
-
     def build(self, platform='windows'):
-        log('Using %s as temporary directory' % self.workdir)
-        # checkout Red5 from repository
-        for path, url in REPOSITORIES.items():
-            self.checkout(url, os.path.join(self.workdir, path))
-        
-        # generate red5.jar
-        log('Building red5.jar')
-        red5_root = os.path.join(self.workdir, 'red5_server')
-        self.compile(self.ant_cmd, os.path.join(red5_root, 'build.xml'), 'jar')
+        log('Building from %s' % self.build_root)
+        # prepare "dist" directory
+        red5_root = os.path.abspath(os.path.join(self.build_root, '..', 'dist'))
+        self.compile(self.ant_cmd, os.path.join(red5_root, '..', 'build.xml'), 'installerdist')
         
         # build installer
         dest = os.getcwd()
-        script = os.path.join(self.build_root, 'red5_bitrock.xml')
-        cmd = BITROCK_CMD
-        try:
-            import win32api
-        except ImportError:
-            pass
-        else:
-            cmd = win32api.GetShortPathName(cmd)
+        args = [
+            '/q',                                   # quiet
+            '/dversion="%s"' % VERSION,             # Red5 version
+            '/droot_dir="%s"' % red5_root,          # Red5 root
+            '/dbuild_dir="%s"' % self.build_root,   # build root
+            '/o"%s"' % dest,                        # output directory
+        ]
+        script = os.path.join(self.build_root, 'red5-setup.iss')
+        cmd = INNO_CMD
+        if ' ' in cmd and not cmd[:1] == '"':
+            cmd = '"' + cmd + '"'
         log('Compiling installer, this may take some time...')
-        assert os.system('%s build %s %s' % (cmd, script, platform)) == 0
-        log('Installer written to %s\setup-red5-%s-%s.*' % (dest, VERSION, platform))
-        
-        # cleanup
-        remove_tree(self.workdir)
+        assert os.system('%s %s %s' % (cmd, script, ' '.join(args))) == 0
+        log('Installer written to %s\setup-red5-%s.exe' % (dest, VERSION))
 
 
 def main():
+    if len(sys.argv) == 2:
+        # create installer with specific version number
+        global VERSION
+        VERSION = sys.argv[1]
+    
     log('Red5 build system')
     log('-----------------')
     JAVA_HOME = os.environ.get('JAVA_HOME', r'C:\Programme\Java\jdk1.5.0_05')
@@ -88,7 +74,6 @@ def main():
         
     log('using "%s" for building' % ANT_CMD)
     
-    #builder = Builder(JAVA_HOME, ANT_CMD, workdir=r'c:\dokume~1\magog\lokale~1\temp\tmpr3bb79red5')
     builder = Builder(JAVA_HOME, ANT_CMD)
     builder.build()
     
