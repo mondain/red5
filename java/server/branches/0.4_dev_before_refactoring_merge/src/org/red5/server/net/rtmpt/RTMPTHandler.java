e428b3abfeae385b52c9f3cbbd463f1014bf929f
lhubbard <lhubbard@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-04-07 06:46:30 +0000

    Creating stable branch for use during merge.

---
+++ java/server/branches/0.4_dev_before_refactoring_merge/src/org/red5/server/net/rtmpt/RTMPTHandler.java
@@ -0,0 +1,49 @@
+package org.red5.server.net.rtmpt;
+
+import org.red5.server.net.SimpleProtocolCodecFactory;
+import org.red5.server.net.rtmp.BaseRTMPHandler;
+import org.red5.server.net.rtmp.message.Constants;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ContextHandler;
+import org.mortbay.xml.XmlConfiguration;
+
+/*
+ * Jetty implementation of the RTMP handler.
+ * 
+ */
+public class RTMPTHandler extends BaseRTMPHandler implements Constants {
+
+	protected static Log log =
+        LogFactory.getLog(RTMPTHandler.class.getName());
+	
+	public static final String HANDLER_ATTRIBUTE = "red5.RMPTHandler";
+	
+	public XmlConfiguration rtmptServerConfiguration = null;
+	
+	protected Server rtmptServer = null;
+	protected SimpleProtocolCodecFactory codecFactory = null;
+	
+	public void setCodecFactory(SimpleProtocolCodecFactory factory) {
+		this.codecFactory = factory;		
+	}
+	
+	public SimpleProtocolCodecFactory getCodecFactory() {
+		return this.codecFactory;		
+	}
+	
+	public void setRtmptServerConfiguration(XmlConfiguration config) throws Exception {
+		rtmptServerConfiguration = config;
+		rtmptServer = (Server) config.newInstance();
+		
+		// Setup configuration data in rtmptServer
+		Handler tmp = rtmptServer.getHandler();
+		if (!(tmp instanceof ContextHandler))
+			throw new Exception("Only context handlers supported.");
+		
+		((ContextHandler) tmp).setAttribute(HANDLER_ATTRIBUTE, this);
+	}
+	
+}
