42f204981826bb71430fcfc3142d8d86e9e8ec19
marijanm <marijanm@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-06-27 14:54:27 +0000

    starting AMF3 implementation (defined some additional datatypes for AMF3 and added class skeleton for input/output classes)

--- java/io/trunk/src/org/red5/io/amf3/Input.java
+++ java/io/trunk/src/org/red5/io/amf3/Input.java
@@ -1,103 +1,110 @@
 package org.red5.io.amf3;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.SimpleTimeZone;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.mina.common.ByteBuffer;
 import org.red5.io.object.BaseInput;
 import org.red5.io.object.DataTypes;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.SimpleTimeZone;
+
 /**
- * Input for red5 data types
- * 
+ * Input for red5 data (AMF3) types
+ *
  * @author The Red5 Project (red5@osflash.org)
  * @author Luke Hubbard, Codegent Ltd (luke@codegent.com)
  * @version 0.3
  */
-public class Input extends BaseInput implements org.red5.io.object.Input  {
+public class Input extends BaseInput implements org.red5.io.object.Input {
 
-	
-	protected static Log log =
-        LogFactory.getLog(Input.class.getName());
-	
+
+	protected static Log log = LogFactory.getLog(Input.class.getName());
+
 	protected ByteBuffer buf;
-	
+
 	protected byte currentDataType;
-	
+
 	/**
 	 * Input Constructor
+	 *
 	 * @param buf
 	 */
-	public Input(ByteBuffer buf){
+	public Input(ByteBuffer buf) {
 		super();
 		this.buf = buf;
 	}
 
 	/**
 	 * Reads the data type
+	 *
 	 * @return byte
 	 */
 	public byte readDataType() {
-		
-		if(buf==null){
+
+		if (buf == null) {
 			log.error("Why is buf null?");
 		}
-		
+
 		currentDataType = buf.get();
-		
 		byte coreType;
-		
-		switch(currentDataType){
-		
+
+		switch (currentDataType) {
+
 			case AMF3.TYPE_NULL:
 				coreType = DataTypes.CORE_NULL;
 				break;
-		
+
 			case AMF3.TYPE_INTEGER:
 			case AMF3.TYPE_NUMBER:
 				coreType = DataTypes.CORE_NUMBER;
 				break;
-				
+
 			case AMF3.TYPE_BOOLEAN_TRUE:
 			case AMF3.TYPE_BOOLEAN_FALSE:
 				coreType = DataTypes.CORE_BOOLEAN;
 				break;
-				
+
 			case AMF3.TYPE_STRING:
 				coreType = DataTypes.CORE_STRING;
 				break;
-				
+				//TODO check XML_SPECIAL
+			case AMF3.TYPE_XML:
+			case AMF3.TYPE_XML_SPECIAL:
+				coreType = DataTypes.CORE_XML;
+				break;
 			case AMF3.TYPE_OBJECT:
 				coreType = DataTypes.CORE_OBJECT;
 				break;
-			
+
 			case AMF3.TYPE_ARRAY:
 				// should we map this to list or array?
 				coreType = DataTypes.CORE_ARRAY;
 				break;
-								
+
 			case AMF3.TYPE_DATE:
 				coreType = DataTypes.CORE_DATE;
 				break;
-				
-				
+
+
 			default:
-				log.info("Unknown datatype: "+currentDataType);
+				log.info("Unknown datatype: " + currentDataType);
 				// End of object, and anything else lets just skip
 				coreType = DataTypes.CORE_SKIP;
 				break;
 		}
-		
+
 		return coreType;
 	}
-	
+
 	// Basic
+
 	/**
 	 * Reads a null
+	 *
 	 * @return Object
 	 */
 	public Object readNull() {
@@ -106,6 +113,7 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Reads a boolean
+	 *
 	 * @return boolean
 	 */
 	public Boolean readBoolean() {
@@ -114,26 +122,25 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Reads a Number
+	 *
 	 * @return Number
 	 */
 	public Number readNumber() {
-		if(currentDataType == AMF3.TYPE_NUMBER){
-			double num = buf.getDouble();
-			return new Double(num);
+		if (currentDataType == AMF3.TYPE_NUMBER) {
+			return buf.getDouble();
 		} else {
 			// we are decoding an int
-			// TODO: add calc here for decoding mixed length int
-			return new Integer(0);
+			return readAMF3Integer();
 		}
 	}
 
 	/**
 	 * Reads a string
+	 *
 	 * @return String
 	 */
-	public String readString(){
-		int len = 0;
-		len = buf.getInt();
+	public String readString() {
+		int len = buf.getInt();
 		// shift by one bit ?
 		// is it a reference ? if not continue
 		int limit = buf.limit();
@@ -144,9 +151,10 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 		// save a reference
 		return string;
 	}
-	
+
 	/**
 	 * Returns a date
+	 *
 	 * @return Date
 	 */
 	public Date readDate() {
@@ -157,21 +165,23 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 		This format is “UTC 1970”. Z1 an Z0 for a 16 bit Big Endian number 
 		indicating the indicated time’s timezone in minutes.
 		 */
-		long ms = (long) buf.getDouble();
+		long ms = (long)buf.getDouble();
 		short clientTimeZoneMins = buf.getShort();
 		ms += clientTimeZoneMins * 60 * 1000;
 		Calendar cal = new GregorianCalendar();
 		cal.setTime(new Date(ms - SimpleTimeZone.getDefault().getRawOffset()));
 		Date date = cal.getTime();
-		if(cal.getTimeZone().inDaylightTime(date)){
+		if (cal.getTimeZone().inDaylightTime(date)) {
 			date.setTime(date.getTime() - cal.getTimeZone().getDSTSavings());
 		}
 		return date;
 	}
 
 	// Array
+
 	/**
 	 * Returns an array
+	 *
 	 * @return int
 	 */
 	public int readStartArray() {
@@ -193,18 +203,21 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 	public void skipEndArray() {
 		// SKIP
 	}
-	
+
 	// Object
+
 	/**
 	 * Reads start list
+	 *
 	 * @return int
-	 */	
+	 */
 	public int readStartMap() {
 		return buf.getInt();
 	}
 
 	/**
 	 * Returns a boolean stating whether this has more items
+	 *
 	 * @return boolean
 	 */
 	public boolean hasMoreItems() {
@@ -213,6 +226,7 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Reads the item index
+	 *
 	 * @return int
 	 */
 	public String readItemKey() {
@@ -221,31 +235,36 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Skips item seperator
+	 *
 	 * @return void
 	 */
-	public void skipItemSeparator(){
+	public void skipItemSeparator() {
 		// SKIP
 	}
 
 	/**
 	 * Skips end list
+	 *
 	 * @return void
 	 */
-	public void skipEndMap(){
+	public void skipEndMap() {
 		skipEndObject();
 	}
-	
+
 	// Object
+
 	/**
 	 * Reads start object
+	 *
 	 * @return String
 	 */
 	public String readStartObject() {
-		 return null;
+		return null;
 	}
 
 	/**
 	 * Returns a boolean stating whether there are more properties
+	 *
 	 * @return boolean
 	 */
 	public boolean hasMoreProperties() {
@@ -254,6 +273,7 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Reads property name
+	 *
 	 * @return String
 	 */
 	public String readPropertyName() {
@@ -262,26 +282,30 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Skips property seperator
+	 *
 	 * @return void
 	 */
-	public void skipPropertySeparator(){
+	public void skipPropertySeparator() {
 		// SKIP
 	}
 
 	/**
 	 * Skips end object
+	 *
 	 * @return void
 	 */
 	public void skipEndObject() {
 		// skip two marker bytes
 		// then end of object byte
-		buf.skip(3); 
+		buf.skip(3);
 		//byte nextType = buf.get();
 	}
 
-    // Others
+	// Others
+
 	/**
 	 * Reads xml
+	 *
 	 * @return String
 	 */
 	public String readXML() {
@@ -290,26 +314,65 @@ public class Input extends BaseInput implements org.red5.io.object.Input  {
 
 	/**
 	 * Reads Custom
+	 *
 	 * @return Object
 	 */
-	public Object readCustom(){
+	public Object readCustom() {
 		// Return null for now
 		return null;
 	}
 
 	/**
 	 * Reads Reference
+	 *
 	 * @return Object
 	 */
 	public Object readReference() {
 		return getReference(buf.getShort());
 	}
-	
+
 	/**
 	 * Resets map
+	 *
 	 * @return void
 	 */
-	public void reset(){
+	public void reset() {
 		this.clearReferences();
+	}
+
+	/**
+	 * Parser of AMF3 "compressed" integer data type
+	 *
+	 * @return a converted integer value
+	 * @throws IOException
+	 * @see <a href="http://osflash.org/amf3/parsing_integers">parsing AMF3 integers (external)</a>
+	 */
+	private int readAMF3Integer() {
+		int n = 0;
+		int b = buf.get();
+		int result = 0;
+
+		while ((b & 0x80) != 0 && n < 3) {
+			result <<= 7;
+			result |= (b & 0x7f);
+			b = buf.get();
+			n++;
+		}
+		if (n < 3) {
+			result <<= 7;
+			result |= b;
+		} else {
+			/* Use all 8 bits from the 4th byte */
+			result <<= 8;
+			result |= b;
+
+			/* Check if the integer should be negative */
+			if ((result & 0x10000000) != 0) {
+				/* and extend the sign bit */
+				result |= 0xe0000000;
+			}
+		}
+
+		return result;
 	}
 }
