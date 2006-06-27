e323ef712f03978b1539b501d5065b317abfc680
marijanm <marijanm@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-06-27 14:54:27 +0000

    starting AMF3 implementation (defined some additional datatypes for AMF3 and added class skeleton for input/output classes)

--- java/io/trunk/src/org/red5/io/amf3/AMF3.java
+++ java/io/trunk/src/org/red5/io/amf3/AMF3.java
@@ -2,66 +2,61 @@ package org.red5.io.amf3;
 
 import java.nio.charset.Charset;
 
+/**
+ * AMF3 data type definitions.<br />
+ * For detailed specification please see <strong>specification AMF3</strong> link below.
+ *
+ * @see org.red5.io.amf.AMF
+ * @see <a href="http://osflash.org/amf3/index">specifictation AMF3 (external)</a>
+ * @see <a href="http://osflash.org/amf/astypes">specifictation AMF (external)</a>
+ */
 public class AMF3 {
 
 	public static final Charset CHARSET = Charset.forName("UTF-8");
 	public static final int LONG_STRING_LENGTH = 65535;
-	
+
 	public final static byte TYPE_NULL = 0x01;
-	public final static byte TYPE_BOOLEAN_TRUE = 0x02; 
+	public final static byte TYPE_BOOLEAN_TRUE = 0x02;
 	public final static byte TYPE_BOOLEAN_FALSE = 0x03;
-	public final static byte TYPE_INTEGER = 0x04; 
-	public final static byte TYPE_NUMBER = 0x05; 
+	public final static byte TYPE_INTEGER = 0x04;
+	public final static byte TYPE_NUMBER = 0x05;
 	public final static byte TYPE_STRING = 0x06;
-	
-	public final static byte TYPE_DATE = 0x08; 
-	public final static byte TYPE_ARRAY = 0x09; 
-	public final static byte TYPE_OBJECT = 0x0A; 
-	 
+	//TODO m.j.m  hm..not defined on site, says it's only XML type, so i'll assume it is for the time beeing..
+	public final static byte TYPE_XML_SPECIAL = 0x07;
+	public final static byte TYPE_DATE = 0x08;
+	public final static byte TYPE_ARRAY = 0x09;
+	public final static byte TYPE_OBJECT = 0x0A;
+	public final static byte TYPE_XML = 0x0B;
+	//public final static byte TYPE_YYY = 0x0C;
+	//public final static byte TYPE_ZZZ = 0x0D;
+
+
 	/**
-	 * amf3-data = null | boolean-false | boolean-true | integer | number | string | date | object | array 
-
-null = 0×01
-boolean-false = 0×02
-boolean-true = 0×03 
-
-integer = 0×04 integer-data
-integer-data = 1*4OCTET 
-
-integer-data is probably the single most used item in amf3. To save space it is an integer that can be 0-4 bytes long. The first bit of each byte determines if the next byte is inclued in this integer-data or not.
-Examples:
-0011 0101 = 53
-1000 0001 0101 0100 = 212
-1000 0110 1100 1010 0011 1111 = 107839 
-
-number = 0×05 number-data
-number-data = 8OCTET 
-
-standard java double serialization 
-
-string = 0×06 string-data
-string-data = integer-data [ modified-utf-8 ]
-modified-utf-8 = *OCTET 
-
-The last bit of the integer-data element in string-data identifies if this string is an inline string(1) or a string reference(0).
-String references are references to an already passed string. String indexes start at 0 and are in the order that the inline string is encountered. The index is the first 15 bits of the integer-data.
-The first 15 bits of the ingteger-data specifies the byte length of the string. Those bytes are parsed using modified utf-8. For more on modified utf-8 see modified utf-8 on wikipedia 
-
-date = 0×08 integer-data [ number-data ] 
-
-The last bit of the date-data element in date identifies if this date is an inline date or a date reference. 
-
-array = 0×09 integer-data [ 1OCTET *amf3-data ] 
-
-object = 0x0A integer-data [ class-def ] [ *amf3-data ]
-class-def = string-data [ *string-data ] 
-
-The last bit of the integer-data element in object identifies if this object is an inline object(1) or an object reference(0). Object references are references to an already passed object. Object indexes start at 0 and are in the order that the objects are passed. Object references include dates, arrays, and objects.
-With inline objects, this second to last bit of the integer-data designates whether the object uses is a reference to a previously passed class-def or it is inline. 1 is an inline class-def. 0 is a reference.
-With class-def references the remaining 14 bits of the integer-data element are the index to the class-def as it was passed.
-class-defs then peal off the 3 and 4 to last bits of the integer-data. These are flags that identify the way the object is serialized/deserialized. I will come back to them later.
-The remaining integer-data represents the number of class members that exist. The next element of a class-def is string-data, this is the class name. 
-
+	 * 00 = property list encoding.<br />
+	 * The remaining integer-data represents the number of class members that exist.<br />
+	 * The property names are read as string-data.<br />
+	 * The values are then read as amf3-data.
 	 */
-	
+	public final static byte TYPE_OBJECT_PROPERTY = 0x00;
+	/**
+	 * 01 = single anonymous property. <br />
+	 * A single amf3-data contains a single value. <br />
+	 * The property name should be declared in the class-def reference but this doesn’t seem <b />
+	 * to always be the case - more to come here.
+	 */
+	public final static byte TYPE_OBJECT_ANONYMOUS_PROPERTY = 0x01;
+	/**
+	 * 10 = name value encoding.<br />
+	 * The property names and values are encoded as string-data followed by amf3-data <br />
+	 * until there is an empty string property name.<br />
+	 * If there is a class-def reference there are no property names and the number of values <br />
+	 * is equal to the number of properties in the class-def.
+	 */
+	public final static byte TYPE_OBJECT_VALUE = 0x0A;
+	/**
+	 * 11 = unseen / unknown
+	 */
+	public final static byte TYPE_OBJECT_UNKNOWN = 0x0B;
+
+
 }
--- java/io/trunk/src/org/red5/io/amf3/Output.java
+++ java/io/trunk/src/org/red5/io/amf3/Output.java
@@ -1,5 +1,127 @@
 package org.red5.io.amf3;
 
-public class Output {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.mina.common.ByteBuffer;
 
+import java.util.Date;
+
+/**
+ * AMF3 output writer
+ *
+ * @see  org.red5.io.amf3.AMF3
+ * @see  org.red5.io.amf3.Input
+ * @author The Red5 Project (red5@osflash.org)
+ */
+public class Output implements org.red5.io.object.Output {
+
+	protected static Log log = LogFactory.getLog(Output.class.getName());
+	protected ByteBuffer buffer;
+
+
+	/**
+	 * Constructor of AMF3 output.
+	 *
+	 * @param buffer instance of ByteBuffer
+	 * @see ByteBuffer
+	 */
+	public Output(ByteBuffer buffer) {
+		this.buffer = buffer;
+	}
+
+	public boolean supportsDataType(byte type) {
+		return false;
+	}// Basic Data Types
+
+	public void writeNumber(Number num) {
+
+	}
+
+	public void writeBoolean(Boolean bol) {
+
+	}
+
+	public void writeString(String string) {
+
+	}
+
+	public void writeDate(Date date) {
+
+	}
+
+	public void writeNull() {
+
+	}// Complex Data Types
+
+	public void writeStartArray(int length) {
+
+	}
+
+	public void markElementSeparator() {
+
+	}
+
+	public void markEndArray() {
+
+	}
+
+	public void writeStartMap(int size) {
+
+	}
+
+	public void writeItemKey(String key) {
+
+	}
+
+	public void markItemSeparator() {
+
+	}
+
+	public void markEndMap() {
+
+	}
+
+	public void writeStartObject(String classname) {
+
+	}
+
+	public void writePropertyName(String name) {
+
+	}
+
+	public void markPropertySeparator() {
+
+	}
+
+	public void markEndObject() {
+
+	}
+
+	public void writeXML(String xml) {
+
+	}// Reference to Complex Data Type
+
+	public void writeReference(Object obj) {
+
+	}// Custom datatypes can be handled by
+
+	public boolean isCustom(Object custom) {
+		return false;
+	}
+
+	public void writeCustom(Object custom) {
+
+	}
+
+	public void storeReference(Object obj) {
+
+	}
+
+	public boolean hasReference(Object obj) {
+		return false;
+	}
+
+	public void clearReferences() {
+
+	}
 }
