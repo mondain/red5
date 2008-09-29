f016e01879d81b719acb1403c345012265762880
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2008-09-29 22:11:20 +0000

    Tag for 0.8.0 RC1

---
+++ java/example/tags/0_8rc1/oflaDemo/src/org/red5/demos/oflaDemo/IDemoService.java
@@ -0,0 +1,16 @@
+package org.red5.demos.oflaDemo;
+
+import java.util.Map;
+
+public interface IDemoService {
+
+	/**
+     * Getter for property 'listOfAvailableFLVs'.
+     *
+     * @return Value for property 'listOfAvailableFLVs'.
+     */
+    public Map getListOfAvailableFLVs();
+
+    public Map getListOfAvailableFLVs(String string);
+
+}
