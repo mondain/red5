6f321a9c89e980e51916d6995f72c34bf641e0b4
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-05-16 21:31:57 +0000

    Fix for APPSERVER-383

--- java/example/trunk/oflaDemo/src/org/red5/demos/oflaDemo/DemoServiceImpl.java
+++ java/example/trunk/oflaDemo/src/org/red5/demos/oflaDemo/DemoServiceImpl.java
@@ -3,19 +3,25 @@ package org.red5.demos.oflaDemo;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.red5.logging.Red5LoggerFactory;
+import org.slf4j.Logger;
+
 public class DemoServiceImpl implements IDemoService {
+	
+	private static Logger log = Red5LoggerFactory.getLogger(DemoServiceImpl.class, "oflaDemo");
+	
 	/**
      * Getter for property 'listOfAvailableFLVs'.
      *
      * @return Value for property 'listOfAvailableFLVs'.
      */
-    public Map getListOfAvailableFLVs() {
-    	System.out.println("getListOfAvailableFLVs empty");
-		return new HashMap(1);
+    public Map<String, Map<String, Object>> getListOfAvailableFLVs() {
+    	log.debug("getListOfAvailableFLVs empty");
+		return new HashMap<String, Map<String, Object>>(1);
 	}
 
-    public Map getListOfAvailableFLVs(String string) {
-    	System.out.println("getListOfAvailableFLVs, Got a string: " + string);
+    public Map<String, Map<String, Object>> getListOfAvailableFLVs(String string) {
+    	log.debug("getListOfAvailableFLVs, Got a string: " + string);
     	return getListOfAvailableFLVs();
     }
 
