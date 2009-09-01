134ca40d6c6d13683fb21406430933cf776efbc0
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-09-01 04:44:04 +0000

    Adding branch for example apps using plugins

---
+++ java/example/branches/plugin_testing/oflaDemo/src/org/red5/demos/oflaDemo/Application.java
@@ -0,0 +1,90 @@
+package org.red5.demos.oflaDemo;
+
+import org.red5.server.plugin.*;
+import org.red5.logging.Red5LoggerFactory;
+import org.red5.server.adapter.ApplicationAdapter;
+import org.red5.server.api.IBandwidthConfigure;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.IScope;
+import org.red5.server.api.stream.IServerStream;
+import org.red5.server.api.stream.IStreamCapableConnection;
+import org.red5.server.api.stream.support.SimpleConnectionBWConfig;
+import org.slf4j.Logger;
+
+public class Application extends ApplicationAdapter {
+
+	private IScope appScope;
+
+	private IServerStream serverStream;
+	
+	/** {@inheritDoc} */
+    @Override
+	public boolean appStart(IScope app) {
+	    super.appStart(app);
+		log.info("oflaDemo appStart");
+		System.out.println("oflaDemo appStart");    	
+		appScope = app;
+		
+		//Get plugin
+		AuthPlugin authPlugin = (AuthPlugin) PluginRegistry.getPlugin("authPlugin");
+
+		//Get the handler (application listener in this case) that you want
+		//FMSAuthenticationHandler  authHandler = authPlugin.getFMSAuthenticationHandler();
+		Red5AuthenticationHandler  authHandler = authPlugin.getRed5AuthenticationHandler();
+
+		//Add the handler as a listener for your app (in appStart or where-ever)
+		addListener(authHandler);
+		
+		return true;
+	}
+
+	/** {@inheritDoc} */
+    @Override
+	public boolean appConnect(IConnection conn, Object[] params) {
+		log.info("oflaDemo appConnect");
+		// Trigger calling of "onBWDone", required for some FLV players
+		/*
+		measureBandwidth(conn);
+		if (conn instanceof IStreamCapableConnection) {
+			IStreamCapableConnection streamConn = (IStreamCapableConnection) conn;
+			SimpleConnectionBWConfig bwConfig = new SimpleConnectionBWConfig();
+			bwConfig.getChannelBandwidth()[IBandwidthConfigure.OVERALL_CHANNEL] =
+				1024 * 1024;
+			bwConfig.getChannelInitialBurst()[IBandwidthConfigure.OVERALL_CHANNEL] =
+				128 * 1024;
+			streamConn.setBandwidthConfigure(bwConfig);
+		}
+		*/
+		
+//		if (appScope == conn.getScope()) {
+//			serverStream = StreamUtils.createServerStream(appScope, "live0");
+//			SimplePlayItem item = new SimplePlayItem();
+//			item.setStart(0);
+//			item.setLength(10000);
+//			item.setName("on2_flash8_w_audio");
+//			serverStream.addItem(item);
+//			item = new SimplePlayItem();
+//			item.setStart(20000);
+//			item.setLength(10000);
+//			item.setName("on2_flash8_w_audio");
+//			serverStream.addItem(item);
+//			serverStream.start();
+//			try {
+//				serverStream.saveAs("aaa", false);
+//				serverStream.saveAs("bbb", false);
+//			} catch (Exception e) {}
+//		}
+		 
+		return super.appConnect(conn, params);
+	}
+
+	/** {@inheritDoc} */
+    @Override
+	public void appDisconnect(IConnection conn) {
+		log.info("oflaDemo appDisconnect");
+		if (appScope == conn.getScope() && serverStream != null) {
+			serverStream.close();
+		}
+		super.appDisconnect(conn);
+	}
+}
