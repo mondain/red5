a8db7f68cecdf47d63ddc09072c6e1731769a021
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-10-29 02:34:21 +0000

    Helpful notice for those that try to use ant after installing a pre-built installer version of Red5.

---
+++ build/windows/trunk/bin/ant.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+ECHO Notice: 
+ECHO Ant is not required, you are using a built version of Red5 which was installed using one of our installers. Ant is used for compiling the server, which in this case is already compiled.
+ECHO Go here for more info: http://wiki.red5.org/
+ECHO Thanks
+PAUSE
