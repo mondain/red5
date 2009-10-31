7576d047a7ac83d729e184af800ed3a7f01afce5
Paul Gregoire <mondain@gmail.com>
2009-10-31 18:53:20 +0000

    Removed 9 trailer and added avatar

--- java/example/trunk/oflaDemo/src/org/red5/demos/oflaDemo/Application.java
+++ java/example/trunk/oflaDemo/src/org/red5/demos/oflaDemo/Application.java
@@ -1,13 +1,9 @@
 package org.red5.demos.oflaDemo;
 
-import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
 import org.red5.server.api.stream.IServerStream;
-import org.red5.server.api.stream.IStreamCapableConnection;
-
-import org.slf4j.Logger;
 
 public class Application extends ApplicationAdapter {
 
