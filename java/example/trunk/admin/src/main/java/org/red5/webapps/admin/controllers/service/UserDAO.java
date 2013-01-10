7845894bd5b45c6fcc6c28b87554eecda0390944
Paul Gregoire <mondain@gmail.com>
2013-01-10 03:13:57 +0000

    Fixed issue #99

--- java/example/trunk/admin/src/main/java/org/red5/webapps/admin/controllers/service/UserDAO.java
+++ java/example/trunk/admin/src/main/java/org/red5/webapps/admin/controllers/service/UserDAO.java
@@ -1,3 +1,20 @@
+/*
+ * RED5 Open Source Flash Server - http://code.google.com/p/red5/
+ * 
+ * Copyright 2006-2012 by respective authors (see below). All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package org.red5.webapps.admin.controllers.service;
 
@@ -5,19 +22,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Hashtable;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
-import org.red5.webapps.admin.Application;
-
-import org.springframework.security.GrantedAuthority;
-import org.springframework.security.GrantedAuthorityImpl;
 
 import org.red5.logging.Red5LoggerFactory;
+import org.red5.webapps.admin.UserDatabase;
 import org.slf4j.Logger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
 
 
 /**
@@ -34,18 +44,8 @@ public class UserDAO {
 				
 		Connection conn = null;
 		PreparedStatement stmt = null;
-		try {
-            // JDBC stuff
-			Hashtable  env = new Hashtable();
-			env.put("java.naming.factory.initial", "org.osjava.sj.SimpleContextFactory");
-			env.put("org.osjava.sj.root", "webapps/admin/WEB-INF/classes/config/");
-			env.put("org.osjava.sj.delimiter", "/");
-
-			Context ctx = new InitialContext(env);
-
-            DataSource ds = (DataSource) ctx.lookup("jdbc/admin");
-            
-			conn = ds.getConnection();
+		try {          
+			conn = UserDatabase.getConnection();
 			//make a statement
 			stmt = conn.prepareStatement("INSERT INTO APPUSER (username, password, enabled) VALUES (?, ?, 'enabled')");
 			stmt.setString(1, username);
@@ -67,38 +67,25 @@ public class UserDAO {
 				}
 			}
 			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
+				UserDatabase.recycle(conn);
 			}
 		}
 		return result;
 	}
 	
-	public static UserDetails getUser(String username) {
-		UserDetails details = null;
+	public static AdminUserDetails getUser(String username) {
+		AdminUserDetails details = null;
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
-            // JDBC stuff
-			Hashtable  env = new Hashtable();
-			env.put("java.naming.factory.initial", "org.osjava.sj.SimpleContextFactory");
-			env.put("org.osjava.sj.root", "webapps/admin/WEB-INF/classes/config/");
-			env.put("org.osjava.sj.delimiter", "/");
-
-			Context ctx = new InitialContext(env);
-
-            DataSource ds = (DataSource) ctx.lookup("jdbc/admin");
-			
-			conn = ds.getConnection();
+			conn = UserDatabase.getConnection();
 			//make a statement
 			stmt = conn.prepareStatement("SELECT * FROM APPUSER WHERE username = ?");
 			stmt.setString(1, username);
 			ResultSet rs = stmt.executeQuery();
 			if (rs.next()) {
 				log.debug("User found");			
-				details = new UserDetails();
+				details = new AdminUserDetails();
 				details.setEnabled("enabled".equals(rs.getString("enabled")));
 				details.setPassword(rs.getString("password"));
 				details.setUserid(rs.getInt("userid"));
@@ -131,10 +118,7 @@ public class UserDAO {
 				}
 			}
 			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-				}
+				UserDatabase.recycle(conn);
 			}
 		}
 		return details;
